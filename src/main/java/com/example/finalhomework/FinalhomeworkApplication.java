b31dcab606e18c73a8519454e817ff544906bb03
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-09-03 16:00:10 +0900

    Initial Setting

---
+++ src/main/java/com/example/finalhomework/FinalhomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalhomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalhomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finalhomework/FinalhomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalhomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
